4041947a24f16330ec26d6446f4651a7d909e50b
Arif Muradli <106350355+arifmuradli@users.noreply.github.com>
2024-08-26 15:52:57 +0400

    Update 2. Replace your single server script for second failover.ps1

--- File System Operations/DFS/2. Replace your single server script for second failover.ps1
+++ File System Operations/DFS/2. Replace your single server script for second failover.ps1
@@ -1,1 +1,30 @@
+###Before running this script your first script should look like this
+####New-DfsnFolder -Path '\\nameserver.domain.local\BranchA\Folder1'  -EnableTargetFailback $true  -TargetPath '\\fileserver-NODE01-P\BranchA\Folder1' -ReferralPriorityClass GlobalHigh -ReferralPriorityRank 0
 
+# Define the path to the file
+$filePath = "C:\Path\To\Your\File.txt"
+
+# Check if the file exists
+if (Test-Path $filePath) {
+    # Read the content of the file
+    $fileContent = Get-Content $filePath
+    
+    # Perform the replacements
+    $fileContent = $fileContent -replace 'New-DfsnFolder', 'New-DfsnFolderTarget'
+    $fileContent = $fileContent -replace ' -EnableTargetFailback \$true ', ' '
+    $fileContent = $fileContent -replace 'NODE01', 'NODE02'
+    $fileContent = $fileContent -replace 'GlobalHigh', 'GlobalLow'
+    $fileContent = $fileContent -replace 'ReferralPriorityRank 0', 'ReferralPriorityRank 5'
+    
+    # Save the modified content back to the file
+    Set-Content -Path $filePath -Value $fileContent
+    
+    Write-Host "File modifications completed successfully."
+} else {
+    Write-Host "File not found at path: $filePath"
+}
+
+
+
+###After implementing this script your output file will look like this
+####New-DfsnFolderTarget -Path '\\nameserver.domain.local\BranchA\Folder1' -TargetPath '\\fileserver-NODE02-P\BranchA\Folder1'  -ReferralPriorityClass GlobalLow  -ReferralPriorityRank 5
