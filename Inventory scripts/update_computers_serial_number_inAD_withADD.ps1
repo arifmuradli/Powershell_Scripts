148d665765f3ebb1fe87b66ae56c905ed3aa6cf0
Arif Muradli <106350355+arifmuradli@users.noreply.github.com>
2024-08-26 14:25:08 +0400

    Create update_computers_serial_number_inAD_withADD.ps1

---
+++ Inventory scripts/update_computers_serial_number_inAD_withADD.ps1
@@ -0,0 +1,17 @@
+### This script updates serial number of local computer to AD
+### This script should run from local endpoint
+### This script uses "Replace" parameter instead of ADD parameter
+# Get the serial number of the computer
+$serialNumber = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+
+# Get the current computer's name
+$computerName = $env:COMPUTERNAME
+
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Update the AD computer object with the serial number
+Set-ADComputer -Identity $computerName -Add @{serialNumber=$serialNumber}
+
+# Output the result
+Write-Output "Serial number $serialNumber has been updated for computer $computerName in Active Directory."
